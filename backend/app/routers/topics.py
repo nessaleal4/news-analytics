7d0b898b6c9c8a5d93972b5a4b682129e62ddba9
Nessa <168396378+nessaleal4@users.noreply.github.com>
2025-03-08 04:24:24 -0600

    Update topics.py

--- backend/app/routers/topics.py
+++ backend/app/routers/topics.py
@@ -6,8 +6,8 @@ import os
 import json
 import pandas as pd
 
-from backend.app.config import get_settings
-from backend.app.services.topic_model import get_topics, get_topic_articles
+from app.config import get_settings
+from app.services.topic_model import get_topics, get_topic_articles
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
